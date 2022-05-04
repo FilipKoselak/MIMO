d571056a8af131f514f6a7f0e9b8ca786c4602c1
FilipKoselak <fkoselak@gmail.com>
2022-05-04 22:26:09 +0200

    commit2

--- MIMO/out/webhelp/oxygen-webhelp/app/config.js
+++ MIMO/out/webhelp/oxygen-webhelp/app/config.js
@@ -114,7 +114,7 @@ define(function() {
         	
             if(id === 'index-1' || id === 'index-2' || id === 'index-3' || id === 'stopwords' 
             ||id === 'htmlFileInfoList' || id === 'keywords') {
-                args = '20220504204621';
+                args = '20220504222222';
         	} 
 	
 	        return (url.indexOf('?') === -1 ? '?' : '&') + args;
